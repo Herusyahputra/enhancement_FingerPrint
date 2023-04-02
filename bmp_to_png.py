4eac5a10583e695e711a93a86bd3ef993e791191
何路 | He Lu <herusyahputra55@gmail.com>
2023-04-02 14:04:48 +0800

    Update bmp_to_png.py

--- bmp_to_png.py
+++ bmp_to_png.py
@@ -2,8 +2,8 @@ from PIL import Image
 import os
 
 # Set the input and output folder paths
-input_folder = "/home/moil-dev002/Downloads/finger_print/dataset/eddy/"
-output_folder = "/home/moil-dev002/Downloads/finger_print/dataset/eddy_png/"
+input_folder = "/home/moil-dev002/Downloads/enhancement_FingerPrint/Dataset/eddy/"
+output_folder = "/home/moil-dev002/Downloads/enhancement_FingerPrint/Dataset/eddy_png/"
 
 # Get a list of all BMP files in the input folder
 bmp_files = [f for f in os.listdir(input_folder) if f.endswith('.bmp')]
@@ -20,3 +20,4 @@ for bmp_file in bmp_files:
 
     # Save the image as a PNG file
     bmp_img.save(png_path, 'PNG')
+    print("success convert to png")
